ac2569f9b369876c476282b9fd126bb3a6cbfbbf
louieKay <37403561+louieKay@users.noreply.github.com>
2019-02-17 18:53:55 +0000

    3 page menu

---
+++ EntAppSecond/Pages/FormWS.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace EntAppSecond.Pages
+{
+    public class FormWSModel : PageModel
+    {
+        public string[] Color = { "Red", "Yellow", "Back" };
+
+        [BindProperty]
+        public bool[] ColorSelected { get; set; } = { false, false, false };
+
+        [BindProperty]
+        public bool ConsentSelected { get; set; }
+
+        [BindProperty]
+        public string ProductCode { get; set; } 
+
+        public void OnPost()
+        {
+            
+        }
+    }
+}
