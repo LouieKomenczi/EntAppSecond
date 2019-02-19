1423c1208c4f6080893c4de3344a2056d196c1df
louieKay <37403561+louieKay@users.noreply.github.com>
2019-02-19 20:20:38 +0000

    WorkSheet: Validation - Student

---
+++ EntAppSecond/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntAppSecond.Models
+{
+    public class Student
+    {
+
+        [Required]
+        [Display(Name = "Student ID")]
+        public string StudentID { get; set; } = "";
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; } = "";
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Please enter student ID")]
+        public string LastName { get; set; } = "";
+    }
+}
--- EntAppSecond/Pages/Students/Create.cshtml.cs
+++ EntAppSecond/Pages/Students/Create.cshtml.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EntAppSecond.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace EntAppSecond.Pages
+namespace EntAppSecond.Pages.Students
 {
-    public class FromWSModel : PageModel
+    public class CreateModel : PageModel
     {
+        [BindProperty]
+        public Student Student { get; set; }
+
         public void OnGet()
         {
 
