13fad4933789ca57bca17e180daea1f17bd27e62
louieKay <37403561+louieKay@users.noreply.github.com>
2019-03-03 11:25:36 +0000

    Routing, student details

---
+++ EntAppSecond/Pages/StudentDetails.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using EntAppSecond.Models;
+
+namespace EntAppSecond.Pages
+{
+    public class StudentDetailsModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public string StudentID { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
